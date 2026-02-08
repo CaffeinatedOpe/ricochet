d349732c88de6c9efcac1dfdb413340ab56c9cbd
CaffeinatedOpe <caffeinatedope@proton.me>
2026-02-08 11:14:28 -0600

    dejank, but actaully works lmao

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use std::io::{ self, Write };
 
 static CONFIG_PATH: &str = "/config/config.toml";
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Config {
 	paths: Table,
 	#[serde(default)]
 	behaviors: Behaviors,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Behaviors {
 	default_page: String,
 }
@@ -29,13 +29,12 @@ impl Default for Behaviors {
 #[get("/{key}")]
 async fn handler(path: web::Path<String>, data: Data<Mutex<Config>>) -> impl Responder {
 	let key = path.into_inner();
-	let config = data.lock().unwrap();
-	if config.paths.contains_key(&key) {
-		Redirect::to(PATHS[&key].as_str().unwrap()).permanent()
+	let mut passed_config = data.lock().unwrap();
+	let mut output_path = passed_config.behaviors.default_page.clone();
+	if passed_config.paths.contains_key(&key) {
+		output_path = passed_config.paths[&key].as_str().unwrap().to_string();
 	}
-	else{
-		Redirect::to(CONFIG.behaviors.default_page.as_str()).permanent()
-	}
+	Redirect::to(output_path).permanent()
 }
 
 #[actix_web::main]
@@ -43,12 +42,12 @@ async fn main() -> std::io::Result<()> {
 	let config_str = std::fs::read_to_string(CONFIG_PATH.to_string()).expect("invalid config");
 	let config: Config = toml::from_str(&config_str).expect("invalid config");
 
-	let data = Data::new(Mutex::new(config));
+	let data = Data::new(Mutex::new(config.clone()));
 
 	HttpServer::new(move || { App::new()
 		.app_data(Data::clone(&data))
 		.service(handler)
-		.service(web::redirect("/", CONFIG.behaviors.default_page.as_str()))
+		.service(web::redirect("/", config.behaviors.default_page.clone()))
 	})
 		.bind(("0.0.0.0", 8081))?
 		.run().await
